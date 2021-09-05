b15e8dfb49745d25ef319a5d5e6e9669486ce5d8
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2021-09-05 15:38:49 +0200

    Disabled button while waiting for data

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,12 @@
 <template>
-  <div class="container text-center my-5">
-    <h1 class="display-4 py-5">Covid-19 Vaccine Trials</h1>
+  <div class="container my-5">
+    <h1 class="display-4 text-center py-5">Covid-19 Vaccine Trials</h1>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import data from './assets/vaccines.json'
-
-export default {
-  setup() {
-    const getTrials = async () => {
-      try {
-        for (let i = 0; i < data.vaccines.length; i++) {
-          const rawData = await fetch(
-            `https://clinicaltrials.gov/api/query/study_fields?expr=${data.vaccines[i].id}&fields=Condition,OfficialTitle,Phase,StartDate,CompletionDate,LastUpdatePostDate,EnrollmentCount,NCTId&fmt=json`
-          ).then((res) => res.json())
-
-          const trialData = rawData.StudyFieldsResponse.StudyFields
-
-          // Check active/completed status
-          let active = 0
-          let completed = 0
-
-          for (let i = 0; i < trialData.length; i++) {
-            if (new Date(trialData[i].CompletionDate) - new Date() >= 0) {
-              active++
-            } else {
-              completed++
-            }
-          }
-
-          data.vaccines[i].trials = trialData
-          data.vaccines[i].activeTrials = active
-          data.vaccines[i].completedTrials = completed
-
-          sessionStorage.setItem(
-            data.vaccines[i].name,
-            JSON.stringify(data.vaccines[i])
-          )
-        }
-      } catch (error) {
-        console.error(error.message)
-      }
-    }
-
-    // TODO: Check sessionStorage content
-    if (sessionStorage.length === 0) {
-      console.log('Get data from API...')
-      getTrials()
-    } else {
-      console.log('Data in session storage')
-    }
-
-    return { data }
-  }
-}
+export default {}
 </script>
 
 <style></style>
