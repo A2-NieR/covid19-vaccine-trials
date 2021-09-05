55ca03691c77c442e5026e89da755a6976e262c9
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2021-09-05 16:16:55 +0200

    Center button in details

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <button class="btn btn-primary mb-2" @click="toTrials">Go back</button>
+  <div class="container px-0">
+    <div class="d-flex justify-content-center">
+      <button class="btn btn-primary mb-2" @click="toTrials">Go back</button>
+    </div>
     <div class="d-flex p-3 mt-4 justify-content-evenly alert alert-secondary">
       <div>
         <span class="fw-bold">Vaccine Name: </span><span>{{ vax.name }}</span>
@@ -14,38 +16,40 @@
         <span>{{ vax.type }}</span>
       </div>
     </div>
-    <table class="table table-hover text-start">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Title</th>
-          <th>Phase</th>
-          <th>Start Date</th>
-          <th>Completion Date</th>
-          <th>Last Update</th>
-          <th>Participants</th>
-          <th>NCTId</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="trial in vax.trials" :key="trial.NCTId">
-          <th scope="row">{{ trial.Rank }}</th>
-          <td>{{ trial.OfficialTitle[0] }}</td>
-          <td>{{ trial.Phase[0] }}</td>
-          <td>{{ trial.StartDate[0] }}</td>
-          <td>{{ trial.CompletionDate[0] }}</td>
-          <td>{{ trial.LastUpdatePostDate[0] }}</td>
-          <td>{{ trial.EnrollmentCount[0] }}</td>
-          <td>
-            <a
-              :href="`https://clinicaltrials.gov/ct2/show/study/${trial.NCTId[0]}`"
-              target="_blank"
-              >{{ trial.NCTId[0] }}</a
-            >
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="table-responsive-sm">
+      <table class="table table-hover text-start">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Phase</th>
+            <th>Start Date</th>
+            <th>Completion Date</th>
+            <th>Last Update</th>
+            <th>Participants</th>
+            <th>NCTId</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="trial in vax.trials" :key="trial.NCTId">
+            <th scope="row">{{ trial.Rank }}</th>
+            <td>{{ trial.OfficialTitle[0] }}</td>
+            <td>{{ trial.Phase[0] }}</td>
+            <td>{{ trial.StartDate[0] }}</td>
+            <td>{{ trial.CompletionDate[0] }}</td>
+            <td>{{ trial.LastUpdatePostDate[0] }}</td>
+            <td>{{ trial.EnrollmentCount[0] }}</td>
+            <td>
+              <a
+                :href="`https://clinicaltrials.gov/ct2/show/study/${trial.NCTId[0]}`"
+                target="_blank"
+                >{{ trial.NCTId[0] }}</a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
